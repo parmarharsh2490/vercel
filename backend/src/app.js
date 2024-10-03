46126de42ca3b690be2a6a8bdaee53c1468c92da
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-03 23:42:33 +0530

    Refactor cloudinary configuration to use updated environment variable

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
